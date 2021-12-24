2cc8e9df91169a100d1be50424a434b882081ef7
Johnathan Shields <Johnathan.Shields@buhlergroup.com>
2021-12-24 14:26:26 -0800

    testing for loop

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -2,6 +2,16 @@
 var currentDay = moment().local().format("dddd, MMMM Do");
 console.log (currentDay);
 
+// create working hours array
+var workingHours = [9, 10, 11, 12, 1, 2, 3, 4, 5]
+
+// loop through working hours to create elements dynamically
+
+for (i = 0; i < workingHours.length; i++) {
+    //testing if loop is incrementing properly
+    console.log(workingHours[i]);
+}
+
 // dynamically display current date
 var currentDayDisplay = function(){
     $("#currentDay").text(currentDay)
