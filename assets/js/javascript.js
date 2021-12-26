b6d26426ec6a77f03b05b4359fb82a7980ec89fe
Johnathan Shields <Johnathan.Shields@buhlergroup.com>
2021-12-25 22:13:17 -0800

    developed javascript

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -2,10 +2,26 @@
 var currentDay = moment().local().format("dddd, MMMM Do");
 console.log (currentDay);
 
-// create working hours array
-var workingHours = [9, 10, 11, 12, 1, 2, 3, 4, 5]
+// get time slot comparison value
+var comparisonTimeSlot = moment().local().format("hA");
+console.log(comparisonTimeSlot);
 
-// loop through working hours to create elements dynamically
+// get time slot hour value
+var hourOne = document.querySelector("#timeSlotOne").textContent
+var hourTwo = document.querySelector("#timeSlotTwo").textContent
+var hourThree = document.querySelector("#timeSlotThree").textContent
+var hourFour = document.querySelector("#timeSlotFour").textContent
+var hourFive = document.querySelector("#timeSlotFive").textContent
+var hourSix = document.querySelector("#timeSlotSix").textContent
+var hourSeven = document.querySelector("#timeSlotSeven").textContent
+var hourEight = document.querySelector("#timeSlotEight").textContent
+var hourNine = document.querySelector("#timeSlotNine").textContent
+
+
+// create working hours array
+var workingHours = [hourOne, hourTwo, hourThree, hourFour, hourFive, hourSix, hourSeven, hourEight, hourNine];
+
+// loop through working hours 
 
 for (i = 0; i < workingHours.length; i++) {
     //testing if loop is incrementing properly
