71b30c756d663ea9eeb1826a656272d0a353cfa2
Johnathan Shields <Johnathan.Shields@buhlergroup.com>
2021-12-26 11:36:25 -0800

    hours now being get/set by moment, easier for comparing

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -7,16 +7,43 @@ var comparisonTimeSlot = moment().local().format("hA");
 console.log(comparisonTimeSlot);
 
 // get time slot hour value
-var hourOne = document.querySelector("#timeSlotOne").textContent
-var hourTwo = document.querySelector("#timeSlotTwo").textContent
-var hourThree = document.querySelector("#timeSlotThree").textContent
-var hourFour = document.querySelector("#timeSlotFour").textContent
-var hourFive = document.querySelector("#timeSlotFive").textContent
-var hourSix = document.querySelector("#timeSlotSix").textContent
-var hourSeven = document.querySelector("#timeSlotSeven").textContent
-var hourEight = document.querySelector("#timeSlotEight").textContent
-var hourNine = document.querySelector("#timeSlotNine").textContent
+var hourOne = moment().hour(9).format("hA");
+var hourTwo = moment().hour(10).format("hA");
+var hourThree = moment().hour(11).format("hA");
+var hourFour = moment().hour(12).format("hA");
+var hourFive = moment().hour(13).format("hA");
+var hourSix = moment().hour(14).format("hA");
+var hourSeven = moment().hour(15).format("hA");
+var hourEight = moment().hour(16).format("hA");
+var hourNine = moment().hour(17).format("hA");
 
+// dynamically set time slot in html
+var setHourOne = document.querySelector("#timeSlotOne")
+ setHourOne.textContent = hourOne
+
+ var setHourTwo = document.querySelector("#timeSlotTwo")
+ setHourTwo.textContent = hourTwo
+
+ var setHourThree = document.querySelector("#timeSlotThree")
+ setHourThree.textContent = hourThree
+
+ var setHourFour = document.querySelector("#timeSlotFour")
+ setHourFour.textContent = hourFour
+
+ var setHourFive = document.querySelector("#timeSlotFive")
+ setHourFive.textContent = hourFive
+
+ var setHourSix = document.querySelector("#timeSlotSix")
+ setHourSix.textContent = hourSix
+
+ var setHourSeven = document.querySelector("#timeSlotSeven")
+ setHourSeven.textContent = hourSeven
+
+ var setHourEight = document.querySelector("#timeSlotEight")
+ setHourEight.textContent = hourEight
+
+ var setHourNine = document.querySelector("#timeSlotNine")
+ setHourNine.textContent = hourNine
 
 // create working hours array
 var workingHours = [hourOne, hourTwo, hourThree, hourFour, hourFive, hourSix, hourSeven, hourEight, hourNine];
@@ -24,8 +51,9 @@ var workingHours = [hourOne, hourTwo, hourThree, hourFour, hourFive, hourSix, ho
 // loop through working hours 
 
 for (i = 0; i < workingHours.length; i++) {
-    //testing if loop is incrementing properly
-    console.log(workingHours[i]);
+ 
+
+    
     
 }
 
