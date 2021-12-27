5c37c3253d99b108a2282a9a1a29ed46658c503b
Johnathan Shields <Johnathan.Shields@buhlergroup.com>
2021-12-26 17:50:33 -0800

    beginnings of functional moment comparison code

--- README.md
+++ README.md
@@ -3,6 +3,9 @@
 ## Purpose
 Challenge assignment intended to test my ability to manipulate dynamically generated HTML and CSS with jQuery, read into 3rd party API documentation and figure out how to implement the API in my application.
 
+## Challenges
+I have been stuck trying to get moment.js to compare seperate moment values. I plan to come back to this assignment and polish the hour comparison functionality, but I am going to submit what I have for the time being which is the time slots, the date displayed and the basic layout of the webpage according to the mockup.
+
 ## Built With
 *HTML
 *CSS
--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,61 +1,68 @@
 // get current date 
-var currentDay = moment().local().format("dddd, MMMM Do");
-console.log (currentDay);
-
-// get time slot comparison value
-var comparisonTimeSlot = moment().local().format("hA");
-console.log(comparisonTimeSlot);
+var currentDay = moment().format("dddd, MMMM Do");
+console.log(moment())
+// // get time slot comparison value
+var currentHour = moment().format("h");
+Number(currentHour);
+console.log(currentHour);
 
 // get time slot hour value
-var hourOne = moment().hour(9).format("hA");
-var hourTwo = moment().hour(10).format("hA");
-var hourThree = moment().hour(11).format("hA");
-var hourFour = moment().hour(12).format("hA");
-var hourFive = moment().hour(13).format("hA");
-var hourSix = moment().hour(14).format("hA");
-var hourSeven = moment().hour(15).format("hA");
-var hourEight = moment().hour(16).format("hA");
-var hourNine = moment().hour(17).format("hA");
+var hourOne = moment().hour(9).format("h");
+var hourTwo = moment().hour(10).format("h");
+var hourThree = moment().hour(11).format("h");
+var hourFour = moment().hour(12).format("h");
+var hourFive = moment().hour(13).format("h");
+var hourSix = moment().hour(14).format("h");
+var hourSeven = moment().hour(15).format("h");
+var hourEight = moment().hour(16).format("h");
+var hourNine = moment().hour(17).format("h");
 
 // dynamically set time slot in html
 var setHourOne = document.querySelector("#timeSlotOne")
- setHourOne.textContent = hourOne
+ setHourOne.textContent = hourOne + "AM"
 
- var setHourTwo = document.querySelector("#timeSlotTwo")
- setHourTwo.textContent = hourTwo
+var setHourTwo = document.querySelector("#timeSlotTwo")
+ setHourTwo.textContent = hourTwo + "AM"
 
- var setHourThree = document.querySelector("#timeSlotThree")
- setHourThree.textContent = hourThree
+var setHourThree = document.querySelector("#timeSlotThree")
+ setHourThree.textContent = hourThree + "AM"
 
- var setHourFour = document.querySelector("#timeSlotFour")
- setHourFour.textContent = hourFour
+var setHourFour = document.querySelector("#timeSlotFour")
+ setHourFour.textContent = hourFour + "PM"
 
- var setHourFive = document.querySelector("#timeSlotFive")
- setHourFive.textContent = hourFive
+var setHourFive = document.querySelector("#timeSlotFive")
+ setHourFive.textContent = hourFive + "PM"
 
- var setHourSix = document.querySelector("#timeSlotSix")
- setHourSix.textContent = hourSix
+var setHourSix = document.querySelector("#timeSlotSix")
+ setHourSix.textContent = hourSix + "PM"
 
- var setHourSeven = document.querySelector("#timeSlotSeven")
- setHourSeven.textContent = hourSeven
+var setHourSeven = document.querySelector("#timeSlotSeven")
+ setHourSeven.textContent = hourSeven + "PM"
 
- var setHourEight = document.querySelector("#timeSlotEight")
- setHourEight.textContent = hourEight
+var setHourEight = document.querySelector("#timeSlotEight")
+ setHourEight.textContent = hourEight + "PM"
 
- var setHourNine = document.querySelector("#timeSlotNine")
- setHourNine.textContent = hourNine
+var setHourNine = document.querySelector("#timeSlotNine")
+ setHourNine.textContent = hourNine + "PM"
 
 // create working hours array
 var workingHours = [hourOne, hourTwo, hourThree, hourFour, hourFive, hourSix, hourSeven, hourEight, hourNine];
 
 // loop through working hours 
 
-for (i = 0; i < workingHours.length; i++) {
- 
+for (i = 0; i < workingHours.length;) {
 
-    
-    
-}
+  Number(workingHours[i])
+
+    if (currentHour > workingHours[i]){
+        console.log(currentHour + " is after " + workingHours[i])
+    } else if (currentHour === workingHours[i]) {
+        console.log(currentHour + " is equal to " + workingHours[i])
+    } else if (currentHour > workingHours[i]){
+        console.log(currentHour + " is before " + workingHours[i])
+    }
+    i++
+};
 
 // dynamically display current date
 var currentDayDisplay = function(){
